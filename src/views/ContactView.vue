d8cfd564d1810634a2640123dc6f276b843f1ac6
Clément Roig <clm.roig@gmail.com>
2025-11-28 23:23:03 +0100

    feat(contactView)

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ExternalLink from '@/components/ExternalLink.vue'
 
-<template>En construction...</template>
+const getEmail = () => {
+  const parts = ['devolutioncontact', 'gmail', 'com']
+  return `${parts[0]}@${parts[1]}.${parts[2]}`
+}
+</script>
 
-<style scoped></style>
+<template>
+  <p>
+    Send an email to
+    <a :href="`mailto:${getEmail()}`">{{ getEmail() }}</a>
+    or message us on
+    <ExternalLink href="https://facebook.com/devolution.band.fr">Facebook</ExternalLink> or
+    <ExternalLink href="https://instagram.com/devolution_band">Instagram</ExternalLink>.
+  </p>
+</template>
+
+<style scoped>
+p {
+  font-size: 1.4rem;
+  text-align: center;
+}
+</style>
